3bf3ab9d241feef6cca708a3cf0ce54d3aa8df07
lethevu <kevinlethevu@gmail.com>
2020-11-14 17:55:04 -0800

    improved google sign in and add more fields in profile

--- timely-app/screens/SignInScreen.tsx
+++ timely-app/screens/SignInScreen.tsx
@@ -1,178 +1,193 @@
-import { View, StyleSheet, TouchableOpacity, Text, Alert, SafeAreaView } from 'react-native';
-import React, { useState } from 'react'
+import {View, StyleSheet, TouchableOpacity, Text, Alert} from 'react-native';
+import React, { useContext, useState} from 'react'
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
 import firebase from "../fbconfig";
 import * as Google from 'expo-google-app-auth';
+import {AuthContext} from "../providers/AuthProvider"
 
-export const SignInScreen = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+export const SignInScreen = ({navigation}) => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
 
-  const db = firebase.firestore();
+    const db = firebase.firestore();
+    const {currentUser} = useContext(AuthContext);
 
-  function handleSignIn() {
-    try {
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password).then(() => {
-          console.log('SIGNED IN SUCCESSFULLY')
-        }).catch((err) => {
-          Alert.alert('OOPS!', err.message, [{ text: 'close' }])
-          console.log(err)
-        });
-    } catch (err) {
-      console.log(err)
+    function handleSignIn(){
+        try{
+            firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password).then((res) =>{
+                console.log('SIGNED IN SUCCESSFULLY: ', res.user.email)
+                return db.collection('profiles').doc(res.user.uid).update(
+                  {
+                    last_logged_in: Date.now()
+                  }
+                )
+            }).catch((err) => {
+                Alert.alert('OOPS!', err.message, [{text:'OK'}])
+                console.log(err)
+            });
+        } catch(err){
+            console.log(err)
+        }
     }
-  }
 
-  async function handleSignInWithGoogleAsync() {
-    try {
-      const result = await Google.logInAsync({
-        androidClientId: '953645514664-3j4ed83dtshv3bpuagshrq99gbcpa0v6.apps.googleusercontent.com',
-        iosClientId: '953645514664-nabr329ih9vkf53l4ghh5a1060ihaec3.apps.googleusercontent.com',
-        scopes: ['profile', 'email'],
-      });
-
-      if (result.type === 'success') {
-
-        onSignIn(result)
-
-      } else {
-        return { cancelled: true };
-      }
-    } catch (e) {
-      return { error: true };
-    }
-  }
-
-  function onSignIn(googleUser) {
-    console.log('Google Auth Response', googleUser);
-    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-    var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
-      unsubscribe();
-      // Check if we are already signed-in Firebase with the correct user.
-      if (!isUserEqual(googleUser, firebaseUser)) {
-        // Build Firebase credential with the Google ID token.
-        var credential = firebase.auth.GoogleAuthProvider.credential(
-          googleUser.idToken,
-          googleUser.accessToken
-        );
-        // Sign in with credential from the Google user.
-        firebase
-          .auth()
-          .signInAndRetrieveDataWithCredential(credential)
-          .then((res) => {
-            //save user info to firestore
-            if (res.user)
-              return db.collection('profiles').doc(res.user.uid).set(
-                {
-                  email: res.user.email,
-                  first_name: googleUser.user.givenName,
-                  last_name: googleUser.user.familyName,
-                  profile_visibility: true,
-                  profileImgURL: googleUser.user.photoUrl,
-                  status: 0,
-                  notification: false
-                }
-              )
-          }).then(() => {
-            console.log('SIGNED UP WITH GOOGLE PROVIDER SUCCESSFULLY')
-          }).catch(error => {
-            console.log("google provider error:", error);
+    async function handleSignInWithGoogleAsync() {
+        try {
+          const result = await Google.logInAsync({
+            androidClientId: '953645514664-3j4ed83dtshv3bpuagshrq99gbcpa0v6.apps.googleusercontent.com',
+            iosClientId: '953645514664-nabr329ih9vkf53l4ghh5a1060ihaec3.apps.googleusercontent.com',
+            scopes: ['profile', 'email'],
           });
-      } else {
-        console.log('User already signed-in Firebase.');
-      }
-    });
-  }
+      
+          if (result.type === 'success') {
 
-  function isUserEqual(googleUser, firebaseUser) {
-    if (firebaseUser) {
-      var providerData = firebaseUser.providerData;
-      for (var i = 0; i < providerData.length; i++) {
-        if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-          providerData[i].uid === googleUser.getBasicProfile().getId()) {
-          // We don't need to reauth the Firebase connection.
-          return true;
+            onSignIn(result)
+            return result.accessToken;
+          } else {
+            return { cancelled: true };
+          }
+        } catch (e) {
+          return { error: true };
         }
       }
-    }
-    return false;
-  }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Timely</Text>
+      function onSignIn(googleUser) {
+        console.log('Google Auth Response', googleUser);
+            // Check if we are already signed-in Firebase with the correct user.
+            if (!isUserEqual(googleUser, currentUser)) {
+              // Build Firebase credential with the Google ID token.
+              var credential = firebase.auth.GoogleAuthProvider.credential(
+                  googleUser.idToken,
+                  googleUser.accessToken
+              );
+              // Sign in with credential from the Google user.
+              firebase
+                .auth()
+                .signInAndRetrieveDataWithCredential(credential)
+                .then((res) => {
+                  //check if this is new user
+                  if (res.additionalUserInfo.isNewUser) {
+                    //save user info to firestore
+                    return db.collection('profiles').doc(res.user.uid).set(
+                        {
+                            email: res.user.email,
+                            first_name: googleUser.user.givenName,
+                            last_name: googleUser.user.familyName,
+                            profile_visibility: true,
+                            profileImgURL: googleUser.user.photoUrl,
+                            status: 0,
+                            notification: false,
+                            created: Date.now()
+                        }
+                    )
+                  }else{
+                    return db.collection('profiles').doc(res.user.uid).update(
+                      {
+                        last_logged_in: Date.now()
+                      }
+                    )
+                  }
+                }).then(() =>{
+                      console.log('SIGNED IN WITH GOOGLE PROVIDER SUCCESSFULLY')
+                  }).catch(error => {
+                      console.log("google provider error:", error);
+                  });
+            } else {
+              console.log('User already signed-in Firebase.');
+            }
+      }
 
-      <FormInput
-        labelValue={email}
-        onChangeText={(userEmail) => setEmail(userEmail)}
-        placeholderText="Email"
-        iconType="user"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
+      function isUserEqual(googleUser, firebaseUser) {
+        
+        if (firebaseUser) {
+          var providerData = firebaseUser.providerData;
+          for (var i = 0; i < providerData.length; i++) {
+              console.log('user id: ',providerData[i].uid)
+            if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                providerData[i].uid === googleUser.user.id) {
+               console.log('We dont need to reauth the Firebase connection.')
+              // We don't need to reauth the Firebase connection.
+              return true;
+            }
+          }
+        }
+        return false;
+      }
 
-      <FormInput
-        labelValue={password}
-        onChangeText={(userPassword) => setPassword(userPassword)}
-        placeholderText="Password"
-        iconType="lock"
-        secureTextEntry={true}
-      />
-
-      <FormButton
-        buttonTitle="Sign In" onPress={handleSignIn}
-      />
-
-      <TouchableOpacity style={styles.verticalButton} onPress={handleSignInWithGoogleAsync}>
-        <Text style={styles.navButtonText}>Sign In with Google?</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={styles.verticalButton}
-        onPress={() => navigation.navigate('SignUp')}>
-        <Text style={styles.navButtonText}>
-          Don't have an acount? Create here
+    return (
+        <View style={styles.container}>
+        <Text style={styles.text}>Timely</Text>
+  
+        <FormInput
+          labelValue={email}
+          onChangeText={(userEmail) => setEmail(userEmail)}
+          placeholderText="Email"
+          iconType="user"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+  
+        <FormInput
+          labelValue={password}
+          onChangeText={(userPassword)=> setPassword(userPassword)}
+          placeholderText="Password"
+          iconType="lock"
+          secureTextEntry={true}
+        />
+  
+        <FormButton
+          buttonTitle="Sign In" onPress={handleSignIn}
+        />
+  
+        <TouchableOpacity style={styles.verticalButton} onPress={handleSignInWithGoogleAsync}>
+          <Text style={styles.navButtonText}>Sign In with Google?</Text>
+        </TouchableOpacity>
+  
+        <TouchableOpacity
+          style={styles.verticalButton}
+          onPress={() => navigation.navigate('SignUp')}>
+          <Text style={styles.navButtonText}>
+            Don't have an acount? Create here
           </Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  )
+        </TouchableOpacity>
+      </View>
+    )
 };
 
 export default SignInScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f9fafd',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  logo: {
-    height: 150,
-    width: 150,
-    resizeMode: 'cover',
-  },
-  text: {
-    fontFamily: 'Roboto',
-    fontSize: 28,
-    marginBottom: 10,
-    color: '#051d5f',
-  },
-  navButton: {
-    marginTop: 15,
-  },
-  verticalButton: {
-    marginVertical: 35,
-  },
-  navButtonText: {
-    fontSize: 18,
-    fontWeight: '500',
-    color: '#2e64e5',
-    fontFamily: 'Roboto',
-  },
-})
+    container: {
+      backgroundColor: '#f9fafd',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+    },
+    logo: {
+      height: 150,
+      width: 150,
+      resizeMode: 'cover',
+    },
+    text: {
+      fontFamily: 'Roboto',
+      fontSize: 28,
+      marginBottom: 10,
+      color: '#051d5f',
+    },
+    navButton: {
+      marginTop: 15,
+    },
+    verticalButton: {
+      marginVertical: 35,
+    },
+    navButtonText: {
+      fontSize: 18,
+      fontWeight: '500',
+      color: '#2e64e5',
+      fontFamily: 'Roboto',
+    },
+  });
--- timely-app/screens/SignUpScreen.tsx
+++ timely-app/screens/SignUpScreen.tsx
@@ -1,144 +1,143 @@
-import { View, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native';
-import React, { useContext, useState } from 'react'
+import {View, StyleSheet, TouchableOpacity, Text, Alert} from 'react-native';
+import React, {useContext, useState} from 'react'
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
-import firebase from "../fbconfig";
+import firebase from "../fbconfig"; 
 
-export const SignUpScreen = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [comPassword, setComPassword] = useState('');
+export const SignUpScreen = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [comPassword, setComPassword] = useState('');
 
-  const [emailErr, setEmailErr] = useState('');
-  const [passwordErr, setPasswordErr] = useState('');
-  const [comPasswordErr, setComPasswordErr] = useState('');
+    const [emailErr, setEmailErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+    const [comPasswordErr, setComPasswordErr] = useState('');
 
-  const db = firebase.firestore();
+    const db = firebase.firestore();
 
-  function handleSignUp() {
-    if (email.length == 0) {
-      setEmailErr('Email cannot be blank!')
-    } else if (password.length < 6 || comPassword.length < 6) {
-      setPasswordErr('Password must be greater than 5 characters');
-      setComPasswordErr('Password must be greater than 5 characters');
-    } else {
-      try {
-        firebase.auth().createUserWithEmailAndPassword(
-          email,
-          password
-        ).then((res) => {
-          //save user info to firestore
-          if (res.user)
-            return db.collection('profiles').doc(res.user.uid).set(
-
-              {
-                email: res.user.email,
-                profile_visibility: true,
-                status: 0,
-                notification: false
-              }
-            )
-        }).then(() => {
-          console.log('SIGNED UP SUCCESSFULLY')
-        }).catch(function (err) {
-          // Handle Errors here.
-          Alert.alert('OOPS!', err.message, [{ text: 'close' }])
-          console.log(err)
-        });
-      } catch (err) {
-        console.log(err)
-      }
+    function handleSignUp(){
+        if (email.length == 0){
+            setEmailErr('Email cannot be blank!')
+        }else if(password.length < 6 || comPassword.length < 6){
+            setPasswordErr('Password must be greater than 5 characters');
+            setComPasswordErr('Password must be greater than 5 characters');
+        }else{
+            try{
+                firebase.auth().createUserWithEmailAndPassword(
+                    email,
+                    password
+                ).then((res)=>{
+                    //save user info to firestore
+                    return db.collection('profiles').doc(res.user.uid).set(
+                        {
+                            email: res.user.email,
+                            profile_visibility: true,
+                            status: 0,
+                            notification: false,
+                            created: Date.now()
+                        }
+                    )
+                }).then(() =>{
+                    console.log('SIGNED UP SUCCESSFULLY')
+                }).catch(function(err) {
+                    // Handle Errors here.
+                    Alert.alert('OOPS!', err.message, [{text:'OK'}])
+                    console.log(err)
+                });
+            }catch(err){
+                console.log(err)
+            }
+        }
     }
-  }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Create An Account.</Text>
+    return (
+        <View style={styles.container}>
+        <Text style={styles.text}>Create An Account.</Text>
+  
+        <FormInput
+          labelValue={email}
+          onChangeText={(userEmail) => setEmail(userEmail)}
+          placeholderText="Email"
+          iconType="user"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
 
-      <FormInput
-        labelValue={email}
-        onChangeText={(userEmail) => setEmail(userEmail)}
-        placeholderText="Email"
-        iconType="user"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
+        {emailErr ? <Text style={styles.alertText}>{emailErr}</Text> : null}
 
-      {emailErr ? <Text style={styles.alertText}>{emailErr}</Text> : null}
+        <FormInput
+          labelValue={password}
+          onChangeText={(userPassword)=> setPassword(userPassword)}
+          placeholderText="Password"
+          iconType="lock"
+          secureTextEntry={true}
+        />
 
-      <FormInput
-        labelValue={password}
-        onChangeText={(userPassword) => setPassword(userPassword)}
-        placeholderText="Password"
-        iconType="lock"
-        secureTextEntry={true}
-      />
+        {passwordErr ? <Text style={styles.alertText}>{passwordErr}</Text> : null}
 
-      {passwordErr ? <Text style={styles.alertText}>{passwordErr}</Text> : null}
+        <FormInput
+          labelValue={comPassword}
+          onChangeText={(comfirmed)=> setComPassword(comfirmed)}
+          placeholderText="Comfirmed Password"
+          iconType="lock"
+          secureTextEntry={true}
+        />
 
-      <FormInput
-        labelValue={comPassword}
-        onChangeText={(comfirmed) => setComPassword(comfirmed)}
-        placeholderText="Comfirmed Password"
-        iconType="lock"
-        secureTextEntry={true}
-      />
+        {comPasswordErr ? <Text style={styles.alertText}>{comPasswordErr}</Text> : null}
 
-      {comPasswordErr ? <Text style={styles.alertText}>{comPasswordErr}</Text> : null}
-
-      <FormButton
-        buttonTitle="Sign Up" onPress={handleSignUp}
-      />
-
-      <TouchableOpacity
-        style={styles.verticalButton}
-        onPress={() => navigation.navigate('SignIn')}>
-        <Text style={styles.navButtonText}>
-          Have an acount? Sign In
+        <FormButton
+          buttonTitle="Sign Up" onPress={handleSignUp}
+        />
+  
+        <TouchableOpacity
+          style={styles.verticalButton}
+          onPress={() => navigation.navigate('SignIn')}>
+          <Text style={styles.navButtonText}>
+            Have an acount? Sign In
           </Text>
-      </TouchableOpacity>
-    </View>
-  )
+        </TouchableOpacity>
+      </View>
+    )
 };
 
 export default SignUpScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f9fafd',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  logo: {
-    height: 150,
-    width: 150,
-    resizeMode: 'cover',
-  },
-  text: {
-    fontFamily: 'Roboto',
-    fontSize: 28,
-    marginBottom: 10,
-    color: '#051d5f',
-  },
-  navButton: {
-    marginTop: 15,
-  },
-  verticalButton: {
-    marginVertical: 35,
-  },
-  navButtonText: {
-    fontSize: 18,
-    fontWeight: '500',
-    color: '#2e64e5',
-    fontFamily: 'Roboto',
-  },
-  alertText: {
-    marginBottom: 10,
-    color: '#ff7979',
-    fontSize: 12,
-    marginTop: 0,
-  }
-});
+    container: {
+      backgroundColor: '#f9fafd',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+    },
+    logo: {
+      height: 150,
+      width: 150,
+      resizeMode: 'cover',
+    },
+    text: {
+      fontFamily: 'Roboto',
+      fontSize: 28,
+      marginBottom: 10,
+      color: '#051d5f',
+    },
+    navButton: {
+      marginTop: 15,
+    },
+    verticalButton: {
+      marginVertical: 35,
+    },
+    navButtonText: {
+      fontSize: 18,
+      fontWeight: '500',
+      color: '#2e64e5',
+      fontFamily: 'Roboto',
+    },
+    alertText:{
+        marginBottom: 10,
+        color: '#ff7979',
+        fontSize: 12,
+        marginTop: 0,
+    }
+  });
