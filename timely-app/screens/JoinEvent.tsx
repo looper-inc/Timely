4012357d89e093520d8b9322c4c997438735a9dc
kevin z <k1zhu@ucsd.edu>
2020-12-06 15:05:20 -0800

    deleteted console log

--- timely-app/screens/JoinEvent.tsx
+++ timely-app/screens/JoinEvent.tsx
@@ -7,7 +7,7 @@ const JoinEvent = async (friend_id, event_id) => {
 
     try {
         // check if user has already added the event by checking
-        // field firend_id, change line 31 to change identifier
+        // field firend_id, change element.xxx to change identifier
         var members = await db.collection('events').doc(friend_id)
             .collection('list').doc(event_id).collection('members').get()
         const membersId = members.docs.map(doc => doc.data())
@@ -32,18 +32,18 @@ const JoinEvent = async (friend_id, event_id) => {
             members = await db.collection('events').doc(friend_id)
                 .collection('list').doc(event_id).collection('members').get()
             const membersId = members.docs.map(doc => doc.data())
-
+            //get amd write event
             db.collection('events').doc(friend_id)
                 .collection('list').doc(event_id).get().then((doc) => {
                 var data
                 data = doc.data()
                 db.collection('events').doc(user.uid).
                 collection('list').add(data).then((dataRef) => {
-                    console.log("this is a log1" + membersId)
+                    //console.log("this is a log1" + membersId)
                     membersId.forEach(element => {
                         db.collection('events').doc(user.uid).collection('list')
                         .doc(dataRef.id).collection('members').add(element)
-                        console.log("this is a log2" + element)
+                        //console.log("this is a log2" + element)
                     });
                 
                 });                        
