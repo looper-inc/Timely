6be0b2f85fd9192ae20d32a4c95173617c095ed2
seon5168 <46694786+seon5168@users.noreply.github.com>
2020-12-06 04:47:44 +0000

    Update EventsScreen.tsx
    Added EditEventScreen

--- timely-app/screens/EventsScreen.tsx
+++ timely-app/screens/EventsScreen.tsx
@@ -5,7 +5,7 @@ import {
   ActivityIndicator,
   View,
   Alert,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
 import { Text } from "../components/Themed";
 import React, { useState, useEffect, useContext } from "react";
@@ -102,7 +102,7 @@ export const EventsScreen = ({ navigation }) => {
   };
 
   const handleEditEvent = itemDetail => {
-    //navigation.navigate("EditEvent", itemDetail);
+    navigation.navigate("EditEvent", itemDetail);
   };
 
   const handleViewDetail = itemDetail => {
