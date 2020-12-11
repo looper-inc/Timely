118efad982dd56c92f77b26c2a5a01f6383095d8
kevin z <k1zhu@ucsd.edu>
2020-12-11 13:45:37 -0800

    temp push

--- timely-app/screens/FollowingFeedScreen.tsx
+++ timely-app/screens/FollowingFeedScreen.tsx
@@ -6,6 +6,7 @@ import firebase from "../fbconfig"
 import { SafeAreaView } from 'react-navigation';
 import { FlatList } from 'react-native-gesture-handler';
 import EventListItem from "../components/FeedScreen/EventListItem"
+import GoalListItem from "../components/FeedScreen/GoalListItem";
 
 export const followingFeedScreen = ({ navigation }) => {
     const [eventsList, setEventsList] = useState<any[]>([]);
@@ -14,6 +15,8 @@ export const followingFeedScreen = ({ navigation }) => {
 
     const db = firebase.firestore();
     const fStorage = firebase.storage();
+    var loading = false
+    let goalsList: any[] = [];
 
     useEffect(() => {
         try {
@@ -24,20 +27,26 @@ export const followingFeedScreen = ({ navigation }) => {
         }
     }, []);
 
+    const handleEditEvent = itemDetail => {
+        navigation.navigate("EditEvent", itemDetail);
+    };
+
     const retrieveEvents = async () => {
-        
+        let items: any[] = [];
         let eventsList: any[] = [];
+        
+
 
         setIsFetching(true)
         let initialQuery = await db.collection('profiles')
             .doc(currentUser.uid)
             .collection('friend_list');
 
-        let userList = [currentUser.uid];
-
+        let userList = [];
+        // get friends public events
         initialQuery.onSnapshot((snapshot) => {
             if (snapshot.size) {
-
+                loading = true
                 snapshot.forEach(friend => {
                     let fid = friend.data().friend_id;
                     userList.push(fid)
@@ -48,6 +57,11 @@ export const followingFeedScreen = ({ navigation }) => {
 
             userList.forEach(user => {
                 let tempEvents = db.collection('events')
+                    .doc(user)
+                    .collection('list')
+                    .where('public', '==', true);
+
+                let tempGoals = db.collection('goal')
                     .doc(user)
                     .collection('list')
                     .where('public', '==', true);
@@ -63,7 +77,19 @@ export const followingFeedScreen = ({ navigation }) => {
                         setIsFetching(false);
                     }
                 })
+
+                tempGoals.onSnapshot((snap) => {
+                    if (snap.size) {
+                        snap.forEach(goal => {
+                            var a = goalsList.push({ ...goal.data(), id: goal.id, user_id: user });
+                            //console.log(eventsList[a-1].id)
+                            //console.log(eventsList[a-1].user_id)
+                        })
+                    }
+                })
             })
+            //items = eventsList.concat(goalsList);
+
         })
     }
 
@@ -71,33 +97,80 @@ export const followingFeedScreen = ({ navigation }) => {
         navigation.navigate('EditGoal', itemDetail);
     }
 
-    const handleViewDetail = (itemDetail) => {
-        console.log('lol')
-        navigation.navigate('EventDetail', itemDetail);
-    }
+    // const handleViewEventDetail = (itemDetail) => {
+    //     console.log('lol')
+    //     navigation.navigate('EventDetail', itemDetail);
+    // }
 
-    if (eventsList)
-        return (
-            <SafeAreaView style={styles.container}>
-                <FlatList
-                    data={eventsList}
-                    renderItem={({ item }) =>
-                        <EventListItem
-                            itemDetail={item}
-                            onPressDetail={handleDetail}
-                            onPressVewDetail={handleViewDetail}
-                        />}
-                    onEndReachedThreshold={0.1}
-                />
-                {isFetching && <ActivityIndicator size="large" color="#0097e6" />}
-            </SafeAreaView>
-        );
+    const handleViewGoalDetail = itemDetail => {
+        navigation.navigate("GoalDetail", itemDetail);
+      };
+    const handleRemoveGoal = itemDetail => {}
 
-    else return (
+
+    return (
         <SafeAreaView style={styles.container}>
-            <ActivityIndicator size="large" color="#0097e6" />
+            {eventsList ? (
+                <>
+                    {!loading ? (
+                        <>
+                            <FlatList
+                                data={eventsList}
+                                renderItem={({ item }) => (
+                                    <EventListItem
+                                        itemDetail={item}
+                                        onPressDetail={handleEditEvent}
+                                        navigation={navigation}
+                                    />
+                                )}
+                            //onEndReached={() => retrieveMoreData()}
+                            //onEndReachedThreshold={0.1}
+                            />
+                        </>
+                    ) : (
+                            <ActivityIndicator size="large" color="#0097e6" />
+                        )}
+
+                    {isFetching && <ActivityIndicator size="large" color="#0097e6" />}
+                </>
+            ) : (
+                    <>
+                        {loading && <ActivityIndicator size="large" color="#0097e6" />}
+                        {!eventsList && !loading && (
+                            <Text style={styles.noDataText}>No Events Available.</Text>
+                        )}
+                    </>
+                )}
+
+            {goalsList ? (
+                <>
+                    <FlatList
+                        data={goalsList}
+                        renderItem={({ item }) => (
+                            <GoalListItem
+                                itemDetail={item}
+                                onPressDetail={handleDetail}
+                                onPressVewDetail={handleViewGoalDetail}
+                                onPressRemoveGoal={handleRemoveGoal}
+                            />
+                        )}
+                        //onEndReached={() => retrieveMoreData()}
+                        //onEndReachedThreshold={0.1}
+                    />
+                    {isFetching && <ActivityIndicator size="large" color="#0097e6" />}
+                </>
+            ) : (
+                    <>
+                        {loading && <ActivityIndicator size="large" color="#0097e6" />}
+                        {!goalsList && !loading && (
+                            <Text style={styles.noDataText}>No Goals Available.</Text>
+                        )}
+                    </>
+                )}
         </SafeAreaView>
-    )
+    );
+
+
 }
 
 
@@ -111,6 +184,13 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontWeight: "bold"
+    },
+    noDataText: {
+        fontSize: 16,
+        color: "#f5f6fa",
+        textTransform: "capitalize",
+        fontWeight: "bold",
+        alignSelf: "center"
     }
 });
 
